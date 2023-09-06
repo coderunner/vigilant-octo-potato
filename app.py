b3f8f6465edc2cc00d67c068014ea421cd1750d7
Felix Trepanier <felixtrepanier@gmail.com>
2023-09-06 10:55:06 -0400

    add httponly cookie

--- app.py
+++ app.py
@@ -8,16 +8,20 @@ app = Flask(__name__)
 
 SESSIONS = {}
 SESSION_ID_COOKIE_KEY = 'sessionId'
+HTTP_ONLY_COOKIE_KEY = 'httpOnly'
+
 
 @app.route("/cookies")
 def index():
     sessionId = request.cookies.get(SESSION_ID_COOKIE_KEY)
     if sessionId == None:
         sessionId = str(uuid.uuid4())
-    
+
     cookies = SESSIONS.get(sessionId, 0)
     SESSIONS[sessionId] = cookies + 1
-    response = make_response(render_template( 'cookies/index.html', nb_cookies = SESSIONS[sessionId]))
+    response = make_response(render_template(
+        'cookies/index.html', nb_cookies=SESSIONS[sessionId]))
     response.headers['cache-control'] = 'no-cache'
     response.set_cookie(SESSION_ID_COOKIE_KEY, sessionId)
+    response.set_cookie(HTTP_ONLY_COOKIE_KEY, 'httpOnly', httponly=True)
     return response
